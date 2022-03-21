2647067c59fba0d1bb1140c71a0426132f7ead00
Wei Dai <greendaiwei@gmail.com>
2022-03-21 19:35:19 -0400

    completed function

--- ratings.py
+++ ratings.py
@@ -1,4 +1,20 @@
-"""Restaurant rating lister."""
+def find_restaurant_ratings(filename):
+    """Restaurant rating lister."""
 
-
-# put your code here
+    #open the file
+    restaurant_info = open(filename)
+    dict_restaurant = {}
+    #tokennize the line
+    for line in restaurant_info:
+        line = line.rstrip()
+        list_info = line.split(":")
+        dict_restaurant[list_info[0]] = list_info[1]
+    #restaurant name
+    #rating
+    #create an empty dictionary
+    #put it into the dicctionary
+    #use a for-loop to print the sorted restaurants and print the rating
+    for key in sorted(dict_restaurant):
+        print(f"{key} is rated at {dict_restaurant[key]}")
+    
+# 
