434477f6738a16730aee99844b7f46ab454d5cac
Wei Dai <greendaiwei@gmail.com>
2022-03-21 20:56:24 -0400

    give the user choices

--- ratings.py
+++ ratings.py
@@ -1,9 +1,9 @@
-def find_restaurant_ratings(filename):
+dict_restaurant = {}
+def print_restaurant_ratings(filename):
     """Restaurant rating lister."""
 
     #open the file
     restaurant_info = open(filename)
-    dict_restaurant = {}
     #tokennize the line
     for line in restaurant_info:
         line = line.rstrip()
@@ -16,5 +16,40 @@ def find_restaurant_ratings(filename):
     #use a for-loop to print the sorted restaurants and print the rating
     for key in sorted(dict_restaurant):
         print(f"{key} is rated at {dict_restaurant[key]}")
+
+def enter_new_restaurant():
+    new_restaurant_name = input("Enter restaurant name: ")
+
+    new_restaurant_rating = input("Enter restaurant rating: ")
+    new_restaurant_rating = int(new_restaurant_rating)
+
+    while new_restaurant_rating > 5 or new_restaurant_rating < 1:
+            print("Please enter a rating 1-5")
+            new_restaurant_rating = input("Please input a new rating: ")
+            new_restaurant_rating = int(new_restaurant_rating)
     
-# 
+    
+    dict_restaurant[new_restaurant_name] = new_restaurant_rating
+
+    
+while True:
+    print("1: Print Restaurant Ratings List in Alphabetical Order")
+    print("2: Add a new restaurant")
+    print("Quit")
+
+    answer = input("Please make a selection: ")
+    
+    
+    if answer == "1":
+        print_restaurant_ratings("scores.txt")
+
+    elif answer == "2":
+        enter_new_restaurant()
+    
+    elif answer == "Quit":
+        break
+    else:
+        print("Please select an option '1','2', or 'Quit'")
+
+
+
